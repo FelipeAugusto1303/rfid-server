f8832c9b22d7d247453f9e351c325d80707e3765
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-08-23 10:14:06 -0400

    feat: reading tags rfid and organize in array

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 
-var tags = null
+app.use(
+  cors({
+    origin: '*',
+  })
+)
+
+var tags = []
 
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-        <head>RFID tags</head>
-        <body>
-            <h1>Tags lidas</h1>
-            <p>${JSON.stringify(tags)}</p>
-        </body>
-    </html>`)
+  res.status(200).send(tags)
 })
 
 app.post('/tags-reader', (req, res) => {
-  console.log(req.body)
-  tags = req.body
+  req.body.forEach((element) => {
+    if (tags.length === 0) {
+      tags.push(element)
+    } else if (tags.some((tag) => tag.reading_epc_hex === element.reading_epc_hex)) {
+      const index = tags.findIndex((obj) => {
+        return obj.reading_epc_hex === element.reading_epc_hex
+      })
+      tags.splice(index, 1, element)
+    } else {
+      tags.push(element)
+    }
+  })
+
+  // clearArray(tags)
+  console.log(tags)
   res.status(201).send('OK!')
 })
+
+const clearArray = (array) => {
+  Date.prototype.removeHours = function (h) {
+    this.setTime(this.getTime() - h * 60 * 60 * 1000)
+    return this
+  }
+  array.forEach((element) => {
+    const time = new Date().getTime() - new Date(element.reading_timestamp).removeHours(1).getTime()
+    console.log(time)
+    if (Math.round(time / 1000) > 120) {
+      const index = tags.findIndex((obj) => {
+        return obj.reading_epc_hex === element.reading_epc_hex
+      })
+      tags.splice(index, 1)
+    }
+  })
+}
 
 app.listen(5001)
